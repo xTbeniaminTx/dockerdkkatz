177a9894aa994d5fb49d9d1f01f2a3798241e494
Beniamin Tolan <beniamin.tolan@gmail.com>
2023-01-17 13:10:36 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const HOST = '0.0.0.0';
+const PORT = 80;
+
+app.get('/', (req, res) => {
+    res.send('Hello World from Express!!!');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
